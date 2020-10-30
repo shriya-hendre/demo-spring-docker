1f5e39c78155f28a26cc0780f3d940f7b328ef0f
Shriya Hendre <Shriya.Hendre@vertexinc.com>
2020-10-30 15:55:05 +0530

    first commit

---
+++ src/main/java/com/example/demo/demospringdocker/DemoSpringDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.demospringdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringDockerApplication.class, args);
+	}
+
+}
